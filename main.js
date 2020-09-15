d364b4a81a44da9e02e78d23c55c86084661b194
sanavinaik10 <67157233+sanavinaik10@users.noreply.github.com>
2020-09-15 16:26:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 lily = "";
 dance_monkey = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 function preload(){
     lily = loadSound("Lily.mp3");
     dance_monkey = loadSound("Dance_Monkey.mp3");
@@ -9,6 +13,8 @@ function setup(){
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    posenet = ml5.poseNet(video,modelLoaded);
+    posenet.on('pose', gotPoses);
 }
 function draw(){
     image(video ,0 , 0, 600, 500);
@@ -22,4 +28,18 @@ function stop_song(){
     lily.stop();
     dance_monkey.pause();
     console.log("songs stoped");
+}
+function modelLoaded(){
+    console.log("Model is loaded");
+}
+function gotPoses(result){
+    if(result.length > 0){
+     console.log(result);
+     leftWristX = result[0].pose.leftWrist.x;
+     leftWristY = result[0].pose.leftWrist.y;
+     rightWristX = result[0].pose.rightWrist.x;
+     rightWristY = result[0].pose.rightWrist.y;
+     console.log("leftWristX = " + leftWristX + " leftWristY = " + leftWristY);
+     console.log("rightWristX = " + rightWristX + " rightWristY = " + rightWristY);
+    }
 }
