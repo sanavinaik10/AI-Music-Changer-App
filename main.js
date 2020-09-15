8fb76355c4e50bfe6c554e47b009ba9a8eaf7e72
sanavinaik10 <67157233+sanavinaik10@users.noreply.github.com>
2020-09-15 17:00:52 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
+scoreleftwrist = 0;
+scorerightwrist = 0;
+status = "";
 function preload(){
     lily = loadSound("Lily.mp3");
     dance_monkey = loadSound("Dance_Monkey.mp3");
@@ -18,28 +21,35 @@ function setup(){
 }
 function draw(){
     image(video ,0 , 0, 600, 500);
+    fill("#FF0000");
+    stroke("#FF0000");
+    status = lily.isPlaying();
+    if(scoreleftwrist >= 0.2){
+        circle(leftWristX,leftWristY,20);
+        dance_monkey.stop();
+        if(status == false){
+            lily.play();
+            document.getElementById("song_name").innerHTML = "Playing Now: Lily by Alan Walker";
+        }
+    }
+
 }
-function pause_song(){
-    lily.pause();
-    dance_monkey.pause();
-    console.log("songs paused");
-}
-function stop_song(){
-    lily.stop();
-    dance_monkey.pause();
-    console.log("songs stoped");
-}
+
 function modelLoaded(){
     console.log("Model is loaded");
 }
 function gotPoses(result){
     if(result.length > 0){
      console.log(result);
+     scoreleftwrist =  result[0].pose.keypoints[9].score;
+     scorerightwrist =  result[0].pose.keypoints[10].score;
      leftWristX = result[0].pose.leftWrist.x;
      leftWristY = result[0].pose.leftWrist.y;
      rightWristX = result[0].pose.rightWrist.x;
      rightWristY = result[0].pose.rightWrist.y;
      console.log("leftWristX = " + leftWristX + " leftWristY = " + leftWristY);
      console.log("rightWristX = " + rightWristX + " rightWristY = " + rightWristY);
+     console.log(scoreleftwrist);
+     console.log(scorerightwrist);
     }
 }
